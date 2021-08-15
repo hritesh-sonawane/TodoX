31c65ea661c7e4baf9420acc884e5d606b21da06
hritesh93 <hriteshbebop@gmail.com>
2021-08-16 01:29:34 +0530

    add redux abstractions

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+// Action creators recieve data from the DOM event, format it as formal JSON 'action' object
+
+import { ADD_TODO, SET_FILTER, TOGGLE_TODO } from "../constants/action-types";
+
+let nextTodoId = 0;
+
+export const addTodo = (text) => {
+  return {
+    type: ADD_TODO,
+    id: nextTodoId++,
+    completed: false,
+    text,
+  };
+};
+
+export const setFilter = (filter) => {
+  return {
+    type: SET_FILTER,
+    filter,
+  };
+};
+
+export const toggleTodo = (id) => {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+};
